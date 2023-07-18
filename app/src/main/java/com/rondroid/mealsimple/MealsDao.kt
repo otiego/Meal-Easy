65afeac834f969a6785b4055ad235cc02451acae
Ronnie <ronald.otiego@gmail.com>
2023-07-18 13:51:05 +0300

    add Database Acess Object

--- app/src/main/java/com/rondroid/mealsimple/MealsDao.kt
+++ app/src/main/java/com/rondroid/mealsimple/MealsDao.kt
@@ -1,4 +1,19 @@
 package com.rondroid.mealsimple
 
-class MealsDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MealsDao {
+
+    @Insert
+    suspend fun addMeal(meals: Meals)
+
+    @Query("SELECT * FROM meals ORDER BY id DESC")
+    suspend fun getAllMeal():List<Meals>
+
+    @Delete
+    suspend fun deleteMeal(meals: Meals)
 }
