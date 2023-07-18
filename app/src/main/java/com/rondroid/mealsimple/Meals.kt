56d950f370183af480cfc970cc400e1f3be82d8d
Ronnie <ronald.otiego@gmail.com>
2023-07-18 13:50:11 +0300

    add meals data class

--- app/src/main/java/com/rondroid/mealsimple/Meals.kt
+++ app/src/main/java/com/rondroid/mealsimple/Meals.kt
@@ -1,4 +1,13 @@
 package com.rondroid.mealsimple
 
-class Meals {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Meals(
+    var breakfast:String = "",
+    var lunch:String = "",
+    var dinner:String = ""
+) {
+    @PrimaryKey(autoGenerate = true) var id:Int =0
 }
