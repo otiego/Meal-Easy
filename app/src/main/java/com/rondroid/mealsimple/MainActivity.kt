693e882d1d4f3ebf994c127dd05f4094c3640df9
Ronnie <ronald.otiego@gmail.com>
2023-07-19 17:36:02 +0300

    add integration logic for the recyclerview and add data

--- app/src/main/java/com/rondroid/mealsimple/MainActivity.kt
+++ app/src/main/java/com/rondroid/mealsimple/MainActivity.kt
@@ -5,57 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rondroid.mealsimple.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
-    private var numberClicked = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (numberClicked == 0){
-            display(Meal("Sunday"))
-        }
+        val arrayListDay = arrayListOf<HomeModel>()
 
-        binding.nextTv.setOnClickListener {
-            numberClicked++
+        arrayListDay.add(HomeModel("Sunday"))
+        arrayListDay.add(HomeModel("Monday"))
+        arrayListDay.add(HomeModel("Tuesday"))
+        arrayListDay.add(HomeModel("Wednesday"))
+        arrayListDay.add(HomeModel("Thursday"))
+        arrayListDay.add(HomeModel("Friday"))
+        arrayListDay.add(HomeModel("Saturday"))
 
-            if((binding.breakfastETv.text.isEmpty() || binding.lunchETv.text.isEmpty() || binding.dinnerETv.text.isEmpty())){
-                Toast.makeText(applicationContext,"One or more details is missing!",Toast.LENGTH_LONG).show()
-            }
-            else {
-                when(numberClicked % 7){
-                    0 -> display(Meal("Sunday"))
-                    1 -> display(Meal("Monday"))
-                    2 -> display(Meal("Tuesday"))
-                    3 -> display(Meal("Wednesday"))
-                    4 -> display(Meal("Thursday"))
-                    5 -> display(Meal("Friday"))
-                    6-> display(Meal("Saturday"))
-                }
-            }
-        }
-
-        binding.button.setOnClickListener {
-            val intent =  Intent(applicationContext,Complete::class.java)
-
-            if ((binding.dayTv.text!="Saturday" && (binding.breakfastETv.text.isEmpty() && binding.lunchETv.text.isEmpty() && binding.dinnerETv.text.isEmpty()))) {
-                Log.d("Cont","It worked")
-                Toast.makeText(applicationContext,"Fill in all the details to proceed",Toast.LENGTH_LONG).show()
-            }
-            else
-                startActivity(intent)
-        }
+        val myAdapter = AdapterHome(arrayListDay, context = this)
+        recyclerView1.layoutManager = LinearLayoutManager(this)
+        recyclerView1.adapter = myAdapter
     }
-
-    private fun display(meal: Meal){
-       binding.dayTv.text = meal.days
-    }
-
-    data class Meal(val days:String)
 }
