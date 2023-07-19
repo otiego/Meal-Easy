9c5fef0f9e4d8e960b94d98f879237a33170af29
Ronnie <ronald.otiego@gmail.com>
2023-07-19 10:50:28 +0300

    add day variable to the Meals data Class

--- app/src/main/java/com/rondroid/mealsimple/Meals.kt
+++ app/src/main/java/com/rondroid/mealsimple/Meals.kt
@@ -2,9 +2,11 @@ package com.rondroid.mealsimple
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.MonthDay
 
 @Entity
 data class Meals(
+    var day :String = "",
     var breakfast:String = "",
     var lunch:String = "",
     var dinner:String = ""
